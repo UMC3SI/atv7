0f7c5c28383b86d40ebef569b7d0b68273d79685
GABRIEL FERRI <gabriel.ferri@outlook.com>
2022-04-18 21:45:16 -0300

    Atv7
    11211102207_11211102064

---
+++ ATV7/Aluno.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg11211102207_11211102064;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Aluno {
+    private  double média;
+    private String m1;
+    private String m2;
+    
+    public class Aluno {String m1, string m2) {
+        this.m1 =m1;
+        this.m1 =m1;
+    }
+
+    public void calcular(){
+        double m1 = (double) double.parsedouble(this.m1);
+        double m1 = (double) double.parsedouble(this.m2);
+        this.media = ((m2*m1)/3);  
+    }
+    public void seM1(string m1)(
+        this.m1=m1;
+
+    )
+    public void seM2(string m1)(
+        this.m2=m2;
+    )
+    
+    
+
